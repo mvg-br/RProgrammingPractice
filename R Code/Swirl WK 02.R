f76392fcdce8adbcc0f5b4129d0ae25a2a71cab4
Marcos Vinicius Garcia <marcos03n@gmail.com>
2020-03-04 23:15:00 -0300

    wk 2

---
+++ R Code/Swirl WK 02.R
@@ -0,0 +1,7 @@
+isTRUE(6>4) ##retorna true se a comparacao for verdadeira
+identical("twins","twins") ##retorna true se identicos
+xor(5==6,!FALSE)## retorna True se os elementos forem FxV ou VxF verdadeiro
+which(c(TRUE, FALSE, TRUE))  ## retorna o indice dos vetores True
+any() ##retorna o true se um dos elementos for verdadeiro
+all() ##retorna true se todos forem verdadeiros
+args(function_1) ## retorna os argumentos da funcao 1
